ba7d42ce47bb91f6878be811669989f7a091d3e7
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-07-18 17:18:38 -0500

    9.4 done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -144,6 +144,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+    
   });
 
